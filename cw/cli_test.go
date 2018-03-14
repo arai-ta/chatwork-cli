29e7a7fb7590e3157f59415e9c5807847dbd431b
Arai Takashi <takashi.arai.nz@icloud.com>
2018-03-15 00:15:49 +0900

    Start rewriting cli test

---
+++ cw/cli_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "testing"
+
+    cli "github.com/rendon/testcli"
+)
+
+func TestSmokeRun(t *testing.T) {
+    cli.Run("cw")
+    if !cli.Success() {
+        t.Fatalf("Expected to succeed, but failed: %s", cli.Error())
+    }
+
+    if !cli.StdoutContains("Simple command line tool") {
+        t.Fatalf("Expected to show help, but not matched: %s", cli.Stdout())
+    }
+}
+
